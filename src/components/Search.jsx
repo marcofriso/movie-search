8fed5dd22e43e71c0d523bf0ae0e25a9aff9f2d5
Marco F <marco.friso@gmail.com>
2021-01-12 11:07:12 +0100

    fix: improve form

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,14 +7,57 @@ const Search = (props) => {
   const setType = (data) => props.inputType(data);
 
   return (
-    <div>
+    <form>
       <p>Insert title</p>
-      <input type="text" onChange={(event) => setTitle(event.target.value)} />
+      <input
+        type="text"
+        required
+        onChange={(event) => setTitle(event.target.value)}
+      />
       <p>Year</p>
-      <input type="text" onChange={(event) => setYear(event.target.value)} />
+      <input
+        type="number"
+        min="1900"
+        max="2100"
+        onChange={(event) => setYear(event.target.value)}
+      />
       <p>Type</p>
-      <input type="text" onChange={(event) => setType(event.target.value)} />
-    </div>
+      <div onChange={(event) => setType(event.target.value)}>
+        <input
+          className="form-check-input"
+          type="radio"
+          id="type1"
+          name="type"
+          value=""
+          defaultChecked
+        />
+        Any
+        <input
+          className="form-check-input"
+          type="radio"
+          id="type2"
+          name="type"
+          value="movie"
+        />
+        Movie
+        <input
+          className="form-check-input"
+          type="radio"
+          id="type3"
+          name="type"
+          value="series"
+        />
+        Series
+        <input
+          className="form-check-input"
+          type="radio"
+          id="type4"
+          name="type"
+          value="episode"
+        />
+        Episode
+      </div>
+    </form>
   );
 };
 
